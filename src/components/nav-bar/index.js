bb7789a1a4c2f945a70c88afd7ce9825f1de7ee4
unknown <javacloudmc410@iiht.tech>
2020-10-17 18:23:31 +0530

    5th commit

--- src/components/nav-bar/index.js
+++ src/components/nav-bar/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import {BrowserRouter as Router,Link,Route,Switch} from 'react-router-dom';
 import "./index.css";
+
 export default class NavBar extends Component {
 
   render() {
@@ -8,18 +9,18 @@ export default class NavBar extends Component {
       <div className="layout-column justify-content-center align-items-center">
         <Router>
         <div className="layout-row justify-content-around align-items-center mt-20 links" data-testid="navigation-tabs">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/news">News</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <Link to="/home">Home</Link>
+        <Link to="/news">News</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/about">About</Link>
         </div>
         <div className="card w-20 ma-0">
           <section className="card-text" data-testid="tab-content">
           <Switch>
-        <Route path="/" ><h1>HOME PAGE</h1></Route>
-        <Route path="/news" ><h1>NEWS PAGE</h1></Route>
-        <Route path="/contact"><h1>CONTACT PAGE</h1></Route>
-        <Route path="/about" ><h1>ABOUT PAGE</h1></Route>
+        <Route path="/home" render={() =><h1>HOME PAGE</h1>} />
+        <Route path="/news" render={() => <h1>NEWS PAGE</h1>} />
+        <Route path="/contact" render={() => <h1>CONTACT PAGE</h1>} />
+        <Route path="/about" render={() => <h1>ABOUT PAGE</h1>} />
         </Switch>
           </section>
         </div>
